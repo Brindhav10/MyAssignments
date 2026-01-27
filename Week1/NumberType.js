be9ca06ac4f3299d70e8f998f3c3736c6cd7633a
Brindhav10 <96050167+Brindhav10@users.noreply.github.com>
2026-01-27 09:45:12 +0530

    Week 1 Assignment files

---
+++ Week1/NumberType.js
@@ -0,0 +1,10 @@
+console.log("Find the Number Type")
+let num= 0
+if (num>0) {
+    console.log("Its a Positive Number!!")    
+}
+else if (num<0){
+    console.log("Its a Negative Number!!")
+}
+else
+    console.log("Its Zero !")
---
+++ Week1/OddOrEven.js
@@ -0,0 +1,9 @@
+console.log("Find Odd OR Even Number")
+let num=100
+
+if(num%2 ==0){
+    console.log("Its a Even Number !")
+}else{
+    console.log("Its a Odd Number !")
+}
+   
